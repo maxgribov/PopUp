bb48c189af46956b804152f554a41aa03ca7adbe
Max Gribov <gribov.max@gmail.com>
2024-01-28 17:24:35 +0300

    Remove tests target

--- Package.swift
+++ Package.swift
@@ -17,8 +17,5 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "PopUp"),
-        .testTarget(
-            name: "PopUpTests",
-            dependencies: ["PopUp"]),
     ]
 )
